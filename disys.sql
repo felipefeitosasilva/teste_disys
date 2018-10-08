c091feede18d6e2491d0812e47d73dc2df4e2b6a
felipefeitosasilva <43963924+felipefeitosasilva@users.noreply.github.com>
2018-10-08 12:32:21 -0300

    Update disys.sql

--- disys.sql
+++ disys.sql
@@ -1,17 +1,11 @@
--- --------------------------------------------------------
--- Servidor:                     127.0.0.1
--- Versão do servidor:           10.1.30-MariaDB - mariadb.org binary distribution
--- OS do Servidor:               Win32
--- HeidiSQL Versão:              9.5.0.5278
--- --------------------------------------------------------
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET NAMES utf8 */;
-/*!50503 SET NAMES utf8mb4 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-
--- Exportação de dados foi desmarcado.
-/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
-/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+CREATE TABLE `usuarios_dev` (
+	`id` INT(11) NOT NULL AUTO_INCREMENT,
+	`usuario_nome` VARCHAR(50) NULL DEFAULT '0',
+	`usuario_email` VARCHAR(50) NULL DEFAULT '0',
+	`senha` VARCHAR(50) NULL DEFAULT '0',
+	PRIMARY KEY (`id`)
+)
+COLLATE='latin1_swedish_ci'
+ENGINE=InnoDB
+AUTO_INCREMENT=5
+;
